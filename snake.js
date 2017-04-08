56142e659f1b8c68827b859b6d0cef365ee2e43b
ErandyR <rerandy15@gmail.com>
2017-04-08 14:00:45 -0500

    regresa al punto de inicio cuando pierde el juego

--- snake.js
+++ snake.js
@@ -3,38 +3,43 @@ var snake=document.getElementById("cabezaSerpiente");
 var contenedor=document.getElementById("contenedor");
 var snakeLeft =0;
 var snakeTop=0;
-    function movimientoDeSnake(e){
-      //alert(e.keyCode);
-      if (e.keyCode == 39){
-        snakeLeft += 20;
-        snake.style.marginLeft=snakeLeft + "px";
-         if(snakeLeft> 480){
-          alert("perdiste");
-          
-        }
-      }
-      if(e.keyCode==37){
-        snakeLeft -= 20;
-        snake.style.marginLeft=snakeLeft + "px";
-        if(snakeLeft< 0){
-          alert("perdiste");
-        }
-      }
-      if(e.keyCode==38){
-         snakeTop -= 20;
-         snake.style.marginTop = snakeTop + "px";
-         if(snakeTop< 0){
-          alert("perdiste");
-        }
-      }
-      if(e.keyCode==40){
-         snakeTop += 20;
-        snake.style.marginTop =snakeTop + "px";
-         if(snakeTop> 480){
-          alert("perdiste");
 
-        }
-      }
+function movimientoDeSnake(e){
+  if (e.keyCode == 39){
+    snakeLeft += 20;
+    snake.style.marginLeft = snakeLeft + "px";
+    if(snakeLeft> 480){
+      gameOver();
     }
+  }
+  if(e.keyCode == 37){
+    snakeLeft -= 20;
+    snake.style.marginLeft = snakeLeft + "px";
+    if(snakeLeft < 0){
+      gameOver();
+    }
+  }
+  if(e.keyCode==38){
+   snakeTop -= 20;
+   snake.style.marginTop = snakeTop + "px";
+   if(snakeTop < 0){
+    gameOver();
+   }
+  }
+  if(e.keyCode==40){
+    snakeTop += 20;
+    snake.style.marginTop = snakeTop + "px";
+    if(snakeTop > 480){
+      gameOver();
 
-    document.onkeydown = movimientoDeSnake;
+    }
+  }
+}
+function gameOver(){
+  alert("Game Over");
+
+  snake.style.marginLeft = 0 + "px"; //regresar al punto de inicio
+  snake.style.marginTop = 0 + "px";
+}
+
+document.onkeydown = movimientoDeSnake;
